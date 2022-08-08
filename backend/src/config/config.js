631364200793a941983a77ffc12b3bf55b24947b
julian <juliyachedev@gmail.com>
2022-08-08 16:21:17 -0300

    first configs

---
+++ backend/src/config/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+export const config = {
+    PORT: process.env.PORT,
+    DB: process.env.DB,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST
+}
