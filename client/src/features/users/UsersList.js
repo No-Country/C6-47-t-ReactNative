910873ed93cca4ee47b36d6a46bd7c773ad7f15f
haguerrerob <haguerrerob@gmail.com>
2022-08-17 14:56:19 -0300

    Nueva aplicación client

---
+++ client/src/features/users/UsersList.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+// import tw from 'twrnc'
+
+const UsersList = () => {
+  return (
+    <View>
+      <Text>UsersList</Text>
+    </View>
+  )
+}
+
+export default UsersList
